e6fa07723a7430fdd1501f7c64b9184ac73310d3
Ming-Hsuan Lin <vincent23495380@gmail.com>
2024-10-11 15:55:40 +0800

    Wrap the usage in a function

--- 07. SKAT.R
+++ 07. SKAT.R
@@ -1,6 +1,8 @@
 library(SKAT)
 
+# -----------------------------------------------------------------------------
 #### Prepare the Data for SKAT (Genotype Matrix, Phenotype Vector) #### 
+# -----------------------------------------------------------------------------
 ## Function to extract gene in gene list and corresponding genotype from variant table 
 extract_genotype_filtered <- function(sample_data, gene_list) {
   # Filter the sample data to only include rows where Gene_refgene matches the gene list
@@ -20,10 +22,7 @@ extract_genotype_filtered <- function(sample_data, gene_list) {
 
 ## Function to create genotype matrix and phenotype vector for SKAT
 generate_SKAT_data <- function(case_list, control_list, gene_list) {
-  
-  # Create a vector of genes from the gene list table
-  gene_list <- gene_list$Gene_refgene
-  
+    
   # Initialize lists to store genotype data and all observed variants
   genotype_list <- list()
   all_variants <- c() # Collect all unique variants
@@ -65,30 +64,44 @@ generate_SKAT_data <- function(case_list, control_list, gene_list) {
   return(list(genotype_matrix = genotype_matrix, phenotype_vector = phenotype_vector))
 }
 
-#### SKAT Analysis of the mutation burden of rare non-synonymous variants (missense) in cilium components across DS-ECD and non-DS-ECD samples ####
-## Gene List to analysis (cilium_components in this case)
-cilium_components <- read.csv("../GENE_LIST/cilium_components.txt",
-                              header = FALSE, sep = "\t", 
-                              stringsAsFactors = FALSE, col.names = "Gene_refgene")
+## Function to run SKAT
+run_SKAT <- function(geneList, group1, group2, SKAT_or_SKATO){
+  
+  genelist2analysis <- geneList
+  
+  if (SKAT_or_SKATO == "SKAT") {
+    method2use <- "davies"  # SKAT method
+  } else if (SKAT_or_SKATO == "SKAT-O") {
+    method2use <- "optimal.adj"  # SKAT-O method
+  }
+  
+  ##  Define covariate (for now sample size is too small (n=3) to include covariate)
+  # include sex as covariate (1 = Male, 0 = Female)
+  #sex_vector <- c(1, 0, 0)  # D25029:M, D25046:F, D25007:F
+  # Create a data frame for covariates
+  #covariate_df <- data.frame(sex = sex_vector)
+  
+  ## Generate the genotype matrix and phenotype vector for SKAT analysis
+  skat_data <- generate_SKAT_data(group1, group2, genelist2analysis)
+  
+  ## Run SKAT
+  # Creating the Null Model, which represents the baseline against which the alternative hypothesis
+  # Not using covariate here (~ 1), which come from PLINK PCA and Sex
+  # case-control status (out_type = "D"),  "C" for the continuous outcome and "D" for the dichotomous outcome
+  null_model <- SKAT_Null_Model(skat_data$phenotype_vector ~ 1, out_type = "D")
+  
+  # method="optimal.adj" represent SKAT-O, default= "davies"
+  # weights.beta use default value c(1,25)
+  skat_result <- SKAT(skat_data$genotype_matrix, null_model, kernel="linear.weighted", method=kernel2use, weights.beta=c(1,25))
+}
 
-## Combine rare non-synonymous variants of samples of each group into a list 
-DS_ECD <- list(D25029_MAF0.01_missense, D25046_MAF0.01_missense)
-nonDS_ECD <- list(D25007_MAF0.01_missense)
+# -----------------------------------------------------------------------------
+#### Usage ####
+# -----------------------------------------------------------------------------
+# Example Usage : Mutation burden of exonic and UTR variants in cilium components across DS-ECD and DS-nonECD samples
+# Gene List to analysis from 04. Gene_in_genelists.R
+# Prepare data
+DS_ECD <- list(D25029_variantgroup$exonic,D25046_variantgroup$exonic)
+DS_nonECD <- list(D25165_variantgroup$exonic,D25168_variantgroup$exonic)
 
-##  Define covariate (for now sample size is too small (n=3) to include covariate)
-# include sex as covariate (1 = Male, 0 = Female)
-sex_vector <- c(1, 0, 0)  # D25029:M, D25046:F, D25007:F
-# Create a data frame for covariates
-covariate_df <- data.frame(sex = sex_vector)
-
-## Generate the genotype matrix and phenotype vector for SKAT analysis
-skat_data <- generate_SKAT_data(DS_ECD, nonDS_ECD, cilium_components)
-
-## Run SKAT-O
-# Creating the Null Model, which represents the baseline against which the alternative hypothesis
-# Not using covariate here (~ 1), which come from PLINK PCA and Sex
-# case-control status (out_type = "D"),  "C" for the continuous outcome and "D" for the dichotomous outcome
-null_model <- SKAT_Null_Model(skat_data$phenotype_vector ~ 1, out_type = "D")
-# method="optimal.adj" represent SKAT-O, default= "davies"
-# weights.beta use default value c(1,25)
-skat_result <- SKAT(skat_data$genotype_matrix, null_model,kernel="linear.weighted", method="optimal.adj", weights.beta=c(1,25))
+SKATtest <- run_SKAT(gene_lists$cilium, DS_ECD, DS_nonECD, "SKAT-O")
