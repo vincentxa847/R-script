2836df274606db712b483739a29f1cb12ffe85d4
Ming-Hsuan Lin <vincent23495380@gmail.com>
2024-10-16 18:17:35 +0800

    Update

--- 11.Shared_variant.R
+++ 11.Shared_variant.R
@@ -1,4 +1,5 @@
 library(dplyr) 
+library(tidyr)
 library(openxlsx)
 library(ggplot2)
 library(ggVennDiagram)
@@ -16,7 +17,7 @@ overlap_variant <- function(tables, output_name) {
   
   # Columns to match: "Chr", "Start", "End", "Ref", "Alt"
   # Perform a merge to find the common rows across all tables
-  shared_rows <- Reduce(function(x, y) merge(x, y, by = c("Chr", "Start", "End", "Ref", "Alt")), tables)
+  shared_rows <- Reduce(function(x, y) merge(x, y, by = c("Gene.refgene","Chr", "Start", "End", "Ref", "Alt")), tables)
   
   # Define a list to hold all groups of variants
   variant_groups <- list()
@@ -53,46 +54,58 @@ overlap_variant <- function(tables, output_name) {
 # REMAIN TO UPDATE: EXCEL OUTPUT and GROUP-LEVEL COMPARSION
 # -------------------------------------------------------------------
 DSECD_0.01 <- list(D25029_MAF0.01, D25046_MAF0.01)
-tmp_overlap_variant <- overlap_variant(DSECD_0.01,"../DS-ECD/DSECD_0.01_shareVariant.xlsx")
+DSnonECD_0.01 <- list(D25165_MAF0.01, D25168_MAF0.01)
+tmp_overlap_variant <- overlap_variant(DSnonECD_0.01,"../DS-nonECD/DSnonECD_0.01_shareVariant.xlsx")
 
 # -------------------------------------------------------------------
 # Compare two tables in R and visualize the shared and unique rows
+# need to consider a variant in more then one gene
 # -------------------------------------------------------------------
-# Create the ID column for D25029_MAF0.01
+# Create the ID column for each sample
+# DS-ECD
 D25029_MAF0.01 <- D25029_MAF0.01 %>%
-  mutate(ID = paste(Chr, Start, End, Ref, Alt, sep = "_"))
-
-# Create the ID column for D25046_MAF0.01
+  mutate(ID = paste(Gene.refgene, Chr, Start, End, Ref, Alt, sep = "_"))
 D25046_MAF0.01 <- D25046_MAF0.01 %>%
-  mutate(ID = paste(Chr, Start, End, Ref, Alt, sep = "_"))
-
-# Create the ID column for D25165_MAF0.01
+  mutate(ID = paste(Gene.refgene,Chr, Start, End, Ref, Alt, sep = "_"))
+# DS-nonECD
+D25163_MAF0.01 <- D25163_MAF0.01 %>%
+  mutate(ID = paste(Gene.refgene,Chr, Start, End, Ref, Alt, sep = "_"))
 D25165_MAF0.01 <- D25165_MAF0.01 %>%
-  mutate(ID = paste(Chr, Start, End, Ref, Alt, sep = "_"))
-
-# Create the ID column for D25168_MAF0.01
+  mutate(ID = paste(Gene.refgene,Chr, Start, End, Ref, Alt, sep = "_"))
 D25168_MAF0.01 <- D25168_MAF0.01 %>%
-  mutate(ID = paste(Chr, Start, End, Ref, Alt, sep = "_"))
-
-# Find unique rows in D25046_MAF0.01
-unique_table2 <- anti_join(D25046_MAF0.01, D25029_MAF0.01, by = "ID")
+  mutate(ID = paste(Gene.refgene,Chr, Start, End, Ref, Alt, sep = "_"))
+# nonDS-ECD
+D25007_MAF0.01 <- D25007_MAF0.01 %>%
+  mutate(ID = paste(Gene.refgene,Chr, Start, End, Ref, Alt, sep = "_"))
 
 # Variant Id within group to plot
 venn_data <- list(
-  DS_ECD = c(D25029_MAF0.01$ID,D25046_MAF0.01$ID),
-  DS_nonECD = c(D25165_MAF0.01$ID,D25168_MAF0.01$ID)
+  DS_ECD = c(D25029_MAF0.01$ID, D25046_MAF0.01$ID),
+  DS_nonECD = c(D25163_MAF0.01$ID, D25165_MAF0.01$ID, D25168_MAF0.01$ID),
+  nonDS_ECD = c(D25007_MAF0.01$ID)
 )
 
 # Venn diagram
-# TODO: label position need to be changed
 ggVennDiagram(venn_data, color = "black", 
               lwd = 0.8, lty = 1) + 
-              scale_fill_gradient(low = "#F4FAFE", high = "#4981BF")
+              scale_fill_gradient(low = "#F4FAFE", high = "#4981BF") + 
+              scale_x_continuous(expand = expansion(mult = .2))
 
 
 ### Extract shared and unique variants
-# Find shared rows based on the new ID column
-shared_rows <- inner_join(D25029_MAF0.01, D25046_MAF0.01, by = "ID")
+# Find unique rows in D25046_MAF0.01
+unique_table2 <- anti_join(D25046_MAF0.01, D25029_MAF0.01, by = "ID")
 
-# Find unique rows in D25029_MAF0.01
-unique_table1 <- anti_join(D25029_MAF0.01, D25046_MAF0.01, by = "ID")
+
+# Find shared variants of three groups (6745)
+shared_variants <- Reduce(intersect, venn_data)
+shared_variants <- trimws(shared_variants)
+
+# Loop through each data frame and extract matching rows
+# it seems that multiple rows with same id, which result in more records as expect
+duplicated_ids <- D25029_MAF0.01$ID[duplicated(D25029_MAF0.01$ID)]
+rows_with_duplicates <- D25029_MAF0.01[D25029_MAF0.01$ID %in% duplicated_ids, ]
+
+tmp <- D25029_MAF0.01[D25029_MAF0.01$ID %in% shared_variants,]
+tmp <- tmp[!duplicated(tmp),]
+
