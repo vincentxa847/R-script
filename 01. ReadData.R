b1f937ae73dca2000ca9ffcb386f340002c874bf
Ming-Hsuan Lin <vincent23495380@gmail.com>
2024-10-16 14:56:28 +0800

    Create 01. ReadData.R

---
+++ 01. ReadData.R
@@ -0,0 +1,50 @@
+#### Function to read tsv file that filter was applied using awk command ####
+# Pre-Filter is Quality (Otherinfo9, col 267) ≥ 300 & Het (Het_Percent, col 359) > 15 & MAF (Max Allele Freq, 354) ≤0.01 | ==0 
+ReadData <- function(filePath){
+  
+  # Generate the output name for the cleaned-up data
+  dir <- dirname(filePath)
+  file_name <- basename(filePath)
+  file_prefix <- substr(file_name, 1, 6)  # Extract the first 6 characters, e.g., D25029
+  output_name <- file.path(dir, paste0(file_prefix, ".MAF001.HET15.tsv"))  # Create output file path
+
+  # Read the TSV file (allow missing values and non-standard quotes)
+  # TSV file is generated using awk command, necessary to use [fill = TRUE, quote = ""]
+  tmp <- read.table(filePath, header = TRUE, sep = "\t", fill = TRUE, quote = "")
+  
+  # Columns to move to the front
+  front_columns <- c("Gene.refgene", "ExonicFunc.refgene", "CADD_phred", "REVEL_score", 
+                     "Severity.Score", "Polyphen2_HDIV_score", "Polyphen2_HVAR_score", 
+                     "dbSNP", "ClinVar", "HGMD.Variant.Class", "Max.Allele.Freq", 
+                     "Taiwan.Biobank", "Het_Percent", "Nucleotide", "AAChange")
+  
+  # Get all column names
+  all_columns <- trimws(colnames(tmp))
+  
+  # Determine the other columns
+  other_columns <- setdiff(all_columns, front_columns)
+  
+  # Reorder columns: front_columns first, followed by the remaining columns
+  # Order rows by CADD_phred in descending order
+  tmp_combined_data <- tmp[, c(front_columns, other_columns)]
+  combined_data <- tmp_combined_data[order(as.numeric(tmp_combined_data$CADD_phred), decreasing = TRUE), ]
+  
+  # Variants in multiple overlapping genes were included in each gene separately 
+  combined_data <- combined_data %>%
+    tidyr::separate_rows(Gene.refgene, sep = ";")
+  
+  # Return the cleaned-up and reordered data (optional: write to output file)
+  #write.table(combined_data, file = output_name, col.names=TRUE, row.names=FALSE, sep = "\t")
+  combined_data
+  
+}
+
+# DS-nonECD
+D25163_MAF0.01 = ReadData("../HET15/D25163_22FHL2LT4.hg38_multianno.mviewer.tsv.MAF0.01.HET15.tsv")
+D25165_MAF0.01 = ReadData("../HET15/D25165_22FHL2LT4.hg38_multianno.mviewer.tsv.MAF0.01.HET15.tsv")
+D25168_MAF0.01 = ReadData("../HET15/D25168_22FHL2LT4.hg38_multianno.mviewer.tsv.MAF0.01.HET15.tsv")
+# nonDS-ECD
+D25007_MAF0.01 = ReadData("../HET15/D25007_22CYGGLT4.hg38_multianno.mviewer.tsv.MAF0.01.HET15.tsv")
+# DS-ECD
+D25029_MAF0.01 = ReadData("../HET15/D25029_22FHKYLT4.hg38_multianno.mviewer.tsv.MAF0.01.HET15.tsv") 
+D25046_MAF0.01 = ReadData("../HET15/D25046_22FHKYLT4.hg38_multianno.mviewer.tsv.MAF0.01.HET15.tsv")
