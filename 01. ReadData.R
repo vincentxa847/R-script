96a1b020dd11a1d1f3ffd50508f37360f0df25a5
Ming-Hsuan Lin <vincent23495380@gmail.com>
2024-10-22 13:02:58 +0800

    Update, order by multiple columns

--- 01. ReadData.R
+++ 01. ReadData.R
@@ -16,8 +16,8 @@ ReadData <- function(filePath){
   
   # Columns to move to the front
   front_columns <- c("Gene.refgene", "ExonicFunc.refgene", "CADD_phred", "REVEL_score", 
-                     "Severity.Score", "Polyphen2_HDIV_score", "Polyphen2_HVAR_score", 
-                     "dbSNP", "ClinVar", "HGMD.Variant.Class", "Max.Allele.Freq", 
+                     "Severity.Score", "Polyphen2_HDIV_score", "Polyphen2_HVAR_score",
+                     "SIFT_score", "dbSNP", "ClinVar", "HGMD.Variant.Class", "Max.Allele.Freq", 
                      "Taiwan.Biobank", "Het_Percent", "Nucleotide", "AAChange")
   
   # Get all column names
@@ -27,14 +27,24 @@ ReadData <- function(filePath){
   other_columns <- setdiff(all_columns, front_columns)
   
   # Reorder columns: front_columns first, followed by the remaining columns
-  # Order rows by CADD_phred in descending order
+  # Order by multiple columns
   tmp_combined_data <- tmp[, c(front_columns, other_columns)]
-  combined_data <- tmp_combined_data[order(as.numeric(tmp_combined_data$CADD_phred), decreasing = TRUE), ]
+  combined_data <- tmp_combined_data[order(
+    as.numeric(tmp_combined_data$CADD_phred), 
+    as.numeric(tmp_combined_data$REVEL_score), 
+    as.numeric(tmp_combined_data$Polyphen2_HDIV_score), 
+    as.numeric(tmp_combined_data$SIFT_score),
+    as.numeric(tmp_combined_data$SpliceAI_DS_AG),
+    as.numeric(tmp_combined_data$SpliceAI_DS_AL),
+    as.numeric(tmp_combined_data$SpliceAI_DS_DG),
+    as.numeric(tmp_combined_data$SpliceAI_DS_DL),
+    decreasing = TRUE
+  ), ]
   
   # Variants in multiple overlapping genes were included in each gene separately 
   combined_data <- combined_data %>%
     tidyr::separate_rows(Gene.refgene, sep = ";")
-
+  
   # Ambiguous annotation result in duplicated row, remove it
   combined_data <- combined_data[!duplicated(combined_data),]
   
@@ -45,11 +55,11 @@ ReadData <- function(filePath){
 }
 
 # DS-nonECD
-D25163_MAF0.01 = ReadData("../HET15/D25163_22FHL2LT4.hg38_multianno.mviewer.tsv.MAF0.01.HET15.tsv")
-D25165_MAF0.01 = ReadData("../HET15/D25165_22FHL2LT4.hg38_multianno.mviewer.tsv.MAF0.01.HET15.tsv")
-D25168_MAF0.01 = ReadData("../HET15/D25168_22FHL2LT4.hg38_multianno.mviewer.tsv.MAF0.01.HET15.tsv")
+D25163_MAF0.01 = ReadData("../WGS/HET15/D25163_22FHL2LT4.hg38_multianno.mviewer.tsv.MAF0.01.HET15.tsv")
+D25165_MAF0.01 = ReadData("../WGS/HET15/D25165_22FHL2LT4.hg38_multianno.mviewer.tsv.MAF0.01.HET15.tsv")
+D25168_MAF0.01 = ReadData("../WGS/HET15/D25168_22FHL2LT4.hg38_multianno.mviewer.tsv.MAF0.01.HET15.tsv")
 # nonDS-ECD
-D25007_MAF0.01 = ReadData("../HET15/D25007_22CYGGLT4.hg38_multianno.mviewer.tsv.MAF0.01.HET15.tsv")
+D25007_MAF0.01 = ReadData("../WGS/HET15/D25007_22CYGGLT4.hg38_multianno.mviewer.tsv.MAF0.01.HET15.tsv")
 # DS-ECD
-D25029_MAF0.01 = ReadData("../HET15/D25029_22FHKYLT4.hg38_multianno.mviewer.tsv.MAF0.01.HET15.tsv") 
-D25046_MAF0.01 = ReadData("../HET15/D25046_22FHKYLT4.hg38_multianno.mviewer.tsv.MAF0.01.HET15.tsv")
+D25029_MAF0.01 = ReadData("../WGS/HET15/D25029_22FHKYLT4.hg38_multianno.mviewer.tsv.MAF0.01.HET15.tsv") 
+D25046_MAF0.01 = ReadData("../WGS/HET15/D25046_22FHKYLT4.hg38_multianno.mviewer.tsv.MAF0.01.HET15.tsv")
